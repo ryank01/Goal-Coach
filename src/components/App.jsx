2b1742c512575b70819daaa0030bc4334cde2293
ryan <ryankawall01@gmail.com>
2017-10-15 17:15:28 -0400

    Redux setup
    Created action creater and reducer.
    Connected Redux to App component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {firebaseApp} from '../firebase';
+import '../styles/App.css';
 
 class App extends Component {
+  signOut() {
+    firebaseApp.auth().signOut();
+  }
+
   render() {
     return (
-      <div>App</div>
+      <div className="App">
+          App
+          <div className="button">
+            <button
+              className="btn btn-danger"
+              onClick={() => this.signOut()}
+              >
+              Sign Out
+            </button>
+          </div>
+      </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  console.log('state', state);
+  return {}
+}
+
+export default connect(mapStateToProps, null) (App);
