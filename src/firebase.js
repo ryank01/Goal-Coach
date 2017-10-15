311521c6bed7c4a7663202c939d95e5b776f5ec9
ryan <ryankawall01@gmail.com>
2017-10-14 22:04:24 -0400

    First Commit / Initial set-up Created initital components, app, sign in and sign up. Added firebase config to the application. Added navigation to different pages/components with react-router.

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDDeElwA-w0-bdurib27IPxCmbzpB15dSY",
+  authDomain: "goalcoach-30bf4.firebaseapp.com",
+  databaseURL: "https://goalcoach-30bf4.firebaseio.com",
+  projectId: "goalcoach-30bf4",
+  storageBucket: "goalcoach-30bf4.appspot.com",
+  messagingSenderId: "203500507082"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
